a35ae7160eafe39e61db4c5ead011006d977f843
wilfreidertrochez99 <wilfreidertrochez99@gmail.com>
2025-10-14 20:18:17 -0500

    Main.java
    Esta es la clase principal

--- Main.java
+++ Main.java
@@ -1,1 +1,18 @@
-
+public class Main {
+    public static void main(String[] args) {
+        Dragon dragon = new Dragon("Smaug", 100, 20);
+        Mago mago = new Mago("Gandalf", 80, 15);
+        Guerrero guerrero = new Guerrero("Conan", 120, 18);
+ 
+        Arma espada = new Arma("Espada Legendaria", 5);
+        Arma baston = new Arma("Bastón de Poder", 7);
+ 
+        mago.aprenderHechizo("Bola de fuego");
+        guerrero.equiparArma(espada);
+        mago.equiparArma(baston);
+ 
+        Batalla.iniciar(mago, guerrero);
+        System.out.println("\n----\n");
+        Batalla.iniciar(dragon, mago);
+    }
+}
